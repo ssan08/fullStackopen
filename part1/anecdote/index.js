56d52bc37a45d86a8030d66b2bb7f26522f05ebb
ssan08 <62076870+ssan08@users.noreply.github.com>
2020-04-18 23:17:20 +0300

    Create index.js

---
+++ part1/anecdote/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+var vote = [0, 0, 0, 0, 0, 0]
+
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [m, setm] = useState(0)
+  const [i, seti] = useState(0)
+
+
+  const handlevote = () => {
+    vote[selected] += 1
+    setm(Math.max(vote[0], vote[1], vote[2], vote[3], vote[4], vote[5]))
+    if (vote[0] == m) {
+      seti(0)
+    } else if (vote[1]-1 == m) {
+      seti(1)
+    } else if (vote[2] -1== m) {
+      seti(2)
+    } else if (vote[3] -1== m) {
+      seti(3)
+    } else if (vote[4]-1 == m) {
+      seti(4)
+    } else if (vote[5] -1== m) {
+      seti(5)
+    }
+    console.log(vote)
+    console.log(m, i)
+
+  }
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>
+        {props.anecdotes[selected]}
+      </p>
+      <p>
+        has {props.vote[selected]} votes
+      </p>
+      <button onClick={handlevote}>
+        vote
+      </button>
+      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
+        next anecdote
+      </button>
+      <h1>Anecdote with most votes</h1>
+      <p>
+        {props.anecdotes[i]}
+      </p>
+      <p>
+        has {m} votes
+      </p>
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} vote={vote} />,
+  document.getElementById('root')
+)
